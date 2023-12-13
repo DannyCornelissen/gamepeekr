a2d685a84dd4234ef445218f5f6bfb58fd343fa7
Danny <danny.cornelissen38@gmail.com>
2023-12-13 14:46:45 +0100

    added end to end tests with cypress

---
+++ Website/gamepeekrwebsite/cypress/e2e/PostRequesttest/LoginAndPostNewReviewTest.cy.js
@@ -0,0 +1,42 @@
+import { auth } from "../../../src/Utils/firebase.utils"
+import APILink from "../../../src/Components/ReusableComponents/Config"
+
+
+
+describe('template spec', () => {
+  it('passes', async() => {
+    cy.visit('http://localhost:3000/')
+
+    cy.signInWithEmailAndPassword(auth, 'testUser@test.com', 'password').then(() => {
+      const data = {
+        id: auth.currentUser.uid,
+        userName: auth.currentUser.email
+      }
+        cy.PostData(data,`${APILink}/api/User`)
+    });
+
+
+
+      cy.get('#Menu').should('exist')
+      .find('li').contains('Add new review').should('exist')
+      .click()
+
+      cy.get('#title').type('review of elden ring')
+      cy.get('#reviewText').type('This is my review of elden ring its a fun game but the end boss is boring')
+      cy.get('#rating').type('7{uparrow}')
+      cy.get('#game').type('Elden Ring')
+      cy.get('form').submit()
+
+    cy.wait(1000)
+
+    cy.get('table').find('tr')
+     .contains('review of elden ring').should('exist').click()
+
+     cy.get('body')
+     .contains('This is my review of elden ring its a fun game but the end boss is boring')
+     .should('exist')
+
+
+    
+  })
+})
